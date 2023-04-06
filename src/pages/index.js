3cc374fdb51f814bc1ad93e6774a47ca8ce35136
Eliane Henriqueta <elianehenriqueta@gmail.com>
2023-04-06 15:04:13 -0300

    criar if-else, looping, env, estlizaçao e hoc.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,50 @@
+/* eslint-disable react/jsx-key */
 import { useState } from 'react';
 import Pessoa from '../../componentes/Pessoa';
+import comAutorizacao from '../../hoc/comAutorizacao';
 
-export default function Home() {
-  const [idadeEliane, setIdadeEliane] = useState(24);
+function Home() {
+  const [idadeEliane, setIdadeEliane] = useState(17);
+  const pessoas = [
+    {
+      nomePessoa:'Lais',
+      nomePessoa: 16
+    },
+    {
+      nomePessoa: 'Debora',
+      idade: 17
+    },
+    {
+      nomePessoa: 'Eliane',
+      nomePessoa: 18
+    },
+    {
+      nomePessoa: 'Ana',
+      nomePessoa: 19
+    }
 
+  ];
   const incrementaIdadeEliane = () => {
-    setIdadeEliane(idadeEliane++);
+    setIdadeEliane(idadeEliane + 1);
   }
   return (
     <>
-      <h1>Ola Mundo!</h1>
+      <h1>Ola Mundo! {process.env.NEXT_PUBLIC_TESTE}</h1>
       <Pessoa nome="Eliane" idade={idadeEliane} />
       <Pessoa nome="Ana" idade={40} />
+      {pessoas.map(({ nomePessoa , idadePessoa }, index) => {
+         return <Pessoa 
+         nome ={nomePessoa} 
+         idade={idadePessoa}
+         key={index}
+         />
+          })}
+
       <button onClick={incrementaIdadeEliane}>
         Incrementa idade de Eliane
       </button>
     </>
   )
 }
+
+export default comAutorizacao(Home);
